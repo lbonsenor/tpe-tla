09ae4c36c37ec0d30ecf2552c7e8c2ca726cf1b3
Lautaro Bonseñor <lbonsenor@itba.edu.ar>
2025-05-04 15:28:51 -0300

    ✨  flex-bison compiler + korean romanizer

--- README.md
+++ README.md
@@ -8,3 +8,48 @@
 > - `Camila LEE` | <clee@itba.edu.ar> | Legajo `63382`
 > - `Fernando LI` | <feli@itba.edu.ar> | Legajo `64382`
 > - `Matias RINALDO` | <mrinaldo@itba.edu.ar> | Legajo `60357`
+
+---
+
+## Flex-Bison-Compiler
+
+[![✗](https://img.shields.io/badge/Release-v1.1.0-ffb600.svg?style=for-the-badge)](https://github.com/agustin-golmar/Flex-Bison-Compiler/releases)
+
+[![✗](https://github.com/agustin-golmar/Flex-Bison-Compiler/actions/workflows/pipeline.yaml/badge.svg?branch=production)](https://github.com/agustin-golmar/Flex-Bison-Compiler/actions/workflows/pipeline.yaml)
+
+A base compiler example, developed with Flex and Bison.
+
+- [Proyecto Especial - `LaNgTeX`](#proyecto-especial---langtex)
+  - [Flex-Bison-Compiler](#flex-bison-compiler)
+  - [Environment](#environment)
+  - [CI/CD](#cicd)
+  - [Recommended Extensions](#recommended-extensions)
+
+## Environment
+
+Set the following environment variables to control and configure the behaviour of the application:
+
+|Name|Default|Description|
+|-|:-:|-|
+|`LOG_IGNORED_LEXEMES`|`true`|When `true`, logs all of the ignored lexemes found with Flex at DEBUGGING level. To remove those logs from the console output set it to `false`.|
+|`LOGGING_LEVEL`|`INFORMATION`|The minimum level to log in the console output. From lower to higher, the available levels are: `ALL`, `DEBUGGING`, `INFORMATION`, `WARNING`, `ERROR` and `CRITICAL`.|
+
+## CI/CD
+
+To trigger an automatic integration on every push or PR (_Pull Request_), you must activate _GitHub Actions_ in the _Settings_ tab. Use the following configuration:
+
+|Key|Value|
+|-|-|
+|Actions permissions|Allow all actions and reusable workflows|
+|Artifact and log retention|`30 days`|
+|Fork pull request workflows from outside collaborators|Require approval for all outside collaborators|
+|Workflow permissions|Read repository contents and packages permissions|
+|Allow GitHub Actions to create and approve pull requests|`false`|
+
+After integration is done, change the badges of this `README.md` to point to the new repository.
+
+## Recommended Extensions
+
+- [C/C++](https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools)
+- [CMake Tools](https://marketplace.visualstudio.com/items?itemName=ms-vscode.cmake-tools)
+- [Yash](https://marketplace.visualstudio.com/items?itemName=daohong-emilio.yash)
---
+++ src/main/c/backend/romanizers/korean.c
@@ -0,0 +1,111 @@
+// Online C compiler to run C program online
+#include <locale.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BASE 0xAC00
+#define LAST 0xD7A3
+#define BLOCK 10
+
+#define INITIAL_COUNT 19
+#define MEDIAL_COUNT 21
+#define FINAL_COUNT 28
+
+char* decomposeSyllable(wchar_t syllable){
+    if (syllable < BASE || syllable > LAST){
+        char* ret = (char*)malloc(2 * sizeof(char));
+        ret[0] = (char)syllable;
+        ret[1] = '\0';
+        return ret;
+    }
+
+    int offset = syllable - BASE;
+    
+    int i_index = offset / (MEDIAL_COUNT * FINAL_COUNT);
+    int m_index = (offset % (MEDIAL_COUNT * FINAL_COUNT)) / FINAL_COUNT;
+    int f_index = offset % FINAL_COUNT;
+    
+    /*
+    wchar_t initial_consonant[] =  	{L'ㄱ', L'ㄲ', L'ㄴ', L'ㄷ', 
+                                    	 L'ㄸ', L'ㄹ', L'ㅁ', L'ㅂ', 
+                                    	 L'ㅃ', L'ㅅ', L'ㅆ', L'ㅇ', 
+                                    	 L'ㅈ', L'ㅉ', L'ㅊ', L'ㅋ', 
+                                    	 L'ㅌ', L'ㅍ', L'ㅎ'};
+                                    
+    wchar_t medial_vowel[] =   	{L'ㅏ', L'ㅐ', L'ㅑ', L'ㅒ', 
+                                    	 L'ㅓ', L'ㅔ', L'ㅕ', L'ㅖ', 
+                                    	 L'ㅗ', L'ㅘ', L'ㅙ', L'ㅚ', 
+                                    	 L'ㅛ', L'ㅜ', L'ㅝ', L'ㅞ', 
+                                    	 L'ㅟ', L'ㅠ', L'ㅡ', L'ㅢ', 
+                                    	 L'ㅣ'};
+                                    
+    wchar_t final_consonant[] =	{L' ', L'ㄱ', L'ㄲ', L'ㄳ', 
+                                    	 L'ㄴ', L'ㄵ', L'ㄶ', L'ㄷ', 
+                                    	 L'ㄹ', L'ㄺ', L'ㄻ', L'ㄼ', 
+                                    	 L'ㄽ', L'ㄾ', L'ㄿ', L'ㅀ', 
+                                    	 L'ㅁ', L'ㅂ', L'ㅄ', L'ㅅ', 
+                                    	 L'ㅆ', L'ㅇ', L'ㅈ', L'ㅊ', 
+                                    	 L'ㅋ', L'ㅌ', L'ㅍ', L'ㅎ',};
+    */
+
+    const char *initials[] =  { "g", "kk", "n", "d", 
+                                "tt", "r/l", "m", "b", 
+                                "bb", "s", "ss", "", 
+                                "j", "jj", "ch", "k", 
+                                "t", "p", "h" };
+
+    const char *medials[] =   { "a", "ae", "ya", "yae", 
+                                "eo", "e", "yeo", "ye", 
+                                "o", "wa", "wae", "oe", 
+                                "yo", "u", "wo", "we", 
+                                "wi", "yu", "eu", "ui", 
+                                "i" };
+    
+    const char *finals[] =    { "", "k", "kk", "ks",  
+                                "n", "nj", "nh", "t",  
+                                "l", "lk", "lm", "lb",  
+                                "ls", "lt", "lp", "lh",  
+                                "m", "p", "ps", "s",  
+                                "ss", "ng", "j", "ch",  
+                                "k", "t", "p", "h" };
+    
+
+    char* ret = (char*)malloc(7 * sizeof(char));;
+    if (!ret) return NULL;
+
+    snprintf(ret, 7, "%s%s%s", initials[i_index], medials[m_index], finals[f_index]);
+
+    return ret;
+}
+
+char* romanizeKR(wchar_t* word){
+    char* ret = malloc(BLOCK * sizeof(char));
+    int total_length = 0;
+    int total_size = 10;
+    
+    for (int i = 0; word[i] != L'\0'; i++){
+        char* aux = decomposeSyllable(word[i]);
+        if (aux){
+            total_length += strlen(aux);
+            if (total_length >= total_size){
+                total_size += BLOCK;
+                ret = realloc(ret, total_size);
+            }
+            strcat(ret, aux);
+            free(aux);
+        }
+    }
+    
+    return ret;
+}
+
+int main() {
+    setlocale(LC_ALL, "en_US.UTF-8");
+    wchar_t* word = L"안녕히 가세요";
+    char* s = romanizeKR(word);
+    wprintf(L"Input: %ls\nOutput: %s\n", word, s);
+
+    return 0;
+}
